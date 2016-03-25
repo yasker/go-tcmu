c76e3529fca997b606f01d62fd66c6a8c70ef0c6
Sheng Yang <sheng@yasker.org>
2016-03-25 00:33:31 -0700

    Write seems fine, but read doesn't

--- cfunc.go
+++ cfunc.go
@@ -5,8 +5,10 @@ package main
 #include <stdlib.h>
 #include <stdarg.h>
 #include <poll.h>
+#include <scsi/scsi.h>
 
 #include "libtcmu.h"
+
 
 void errp(const char *fmt, ...)
 {
@@ -73,6 +75,62 @@ int tcmu_wait_for_next_command(struct tcmu_device *dev) {
 		return -1;
 	}
 	return 0;
+}
+
+uint8_t tcmucmd_get_scsi_cmd(struct tcmulib_cmd *cmd) {
+	return cmd->cdb[0];
+}
+
+int tcmucmd_emulate_inquiry(struct tcmulib_cmd *cmd, struct tcmu_device *dev) {
+	return tcmu_emulate_inquiry(dev,
+			cmd->cdb, cmd->iovec, cmd->iov_cnt, cmd->sense_buf);
+}
+
+int tcmucmd_emulate_test_unit_ready(struct tcmulib_cmd *cmd) {
+	return tcmu_emulate_test_unit_ready(cmd->cdb, cmd->iovec,
+			cmd->iov_cnt, cmd->sense_buf);
+}
+
+int tcmucmd_emulate_service_action_in(struct tcmulib_cmd *cmd,
+		uint64_t num_lbas, uint32_t block_size) {
+	if (cmd->cdb[1] == READ_CAPACITY_16) {
+		return tcmu_emulate_read_capacity_16(num_lbas,
+			block_size,
+			cmd->cdb, cmd->iovec, cmd->iov_cnt, cmd->sense_buf);
+	}
+	return TCMU_NOT_HANDLED;
+}
+
+int tcmucmd_emulate_mode_sense(struct tcmulib_cmd *cmd) {
+	return tcmu_emulate_mode_sense(cmd->cdb, cmd->iovec, cmd->iov_cnt, cmd->sense_buf);
+}
+
+int tcmucmd_emulate_mode_select(struct tcmulib_cmd *cmd) {
+	return tcmu_emulate_mode_select(cmd->cdb, cmd->iovec, cmd->iov_cnt, cmd->sense_buf);
+}
+
+int tcmucmd_set_medium_error(struct tcmulib_cmd *cmd) {
+	return tcmu_set_sense_data(cmd->sense_buf, MEDIUM_ERROR, ASC_READ_ERROR, NULL);
+}
+
+uint64_t tcmucmd_get_lba(struct tcmulib_cmd *cmd) {
+	return tcmu_get_lba(cmd->cdb);
+}
+
+uint32_t tcmucmd_get_xfer_length(struct tcmulib_cmd *cmd) {
+	return tcmu_get_xfer_length(cmd->cdb);
+}
+
+void *allocate_buffer(int length) {
+	return calloc(1, length);
+}
+
+int tcmucmd_memcpy_into_iovec(struct tcmulib_cmd *cmd, void *buf, int length) {
+	return tcmu_memcpy_into_iovec(cmd->iovec, cmd->iov_cnt, buf, length);
+}
+
+int tcmucmd_memcpy_from_iovec(struct tcmulib_cmd *cmd, void *buf, int length) {
+	return tcmu_memcpy_from_iovec(buf, length, cmd->iovec, cmd->iov_cnt);
 }
 
 */
--- main.go
+++ main.go
@@ -6,17 +6,34 @@ package main
 
 #include <errno.h>
 #include <stdlib.h>
+#include <scsi/scsi.h>
 #include "libtcmu.h"
 
 extern struct tcmulib_context *tcmu_init();
 extern bool tcmu_poll_master_fd(struct tcmulib_context *cxt);
 extern int tcmu_wait_for_next_command(struct tcmu_device *dev);
 
+extern uint8_t tcmucmd_get_scsi_cmd(struct tcmulib_cmd *cmd);
+
+extern int tcmucmd_emulate_inquiry(struct tcmulib_cmd *cmd, struct tcmu_device *dev);
+extern int tcmucmd_emulate_test_unit_ready(struct tcmulib_cmd *cmd);
+extern tcmucmd_emulate_service_action_in(struct tcmulib_cmd *cmd,
+		uint64_t num_lbas, uint32_t block_size);
+extern int tcmucmd_emulate_mode_sense(struct tcmulib_cmd *cmd);
+extern int tcmucmd_emulate_mode_select(struct tcmulib_cmd *cmd);
+extern int tcmucmd_set_medium_error(struct tcmulib_cmd *cmd);
+extern uint64_t tcmucmd_get_lba(struct tcmulib_cmd *cmd);
+extern uint32_t tcmucmd_get_xfer_length(struct tcmulib_cmd *cmd);
+extern void *allocate_buffer(int length);
+extern int tcmucmd_memcpy_into_iovec(struct tcmulib_cmd *cmd, void *buf, int length);
+extern int tcmucmd_memcpy_from_iovec(struct tcmulib_cmd *cmd, void *buf, int length);
+
 */
 import "C"
 import "unsafe"
 
 import (
+	"io"
 	"os"
 	"strings"
 
@@ -33,7 +50,7 @@ var (
 type TcmuState struct {
 	file      *os.File
 	lbas      int64
-	blockSize int64
+	blockSize int
 }
 
 //export shOpen
@@ -45,7 +62,7 @@ func shOpen(dev *C.struct_tcmu_device) int {
 
 	blockSizeStr := C.CString("hw_block_size")
 	defer C.free(unsafe.Pointer(blockSizeStr))
-	blockSize := int64(C.tcmu_get_attribute(dev, blockSizeStr))
+	blockSize := int(C.tcmu_get_attribute(dev, blockSizeStr))
 	if blockSize == -1 {
 		log.Errorln("Cannot find valid hw_block_size")
 		return -C.EINVAL
@@ -57,20 +74,21 @@ func shOpen(dev *C.struct_tcmu_device) int {
 		log.Errorln("Cannot find valid disk size")
 		return -C.EINVAL
 	}
-	state.lbas = size / state.blockSize
+	state.lbas = size / int64(state.blockSize)
 
 	cfgString := C.GoString(C.tcmu_get_dev_cfgstring(dev))
 	if cfgString == "" {
 		log.Errorln("Cannot find configuration string")
 		return -C.EINVAL
 	}
-	path := strings.TrimLeft(cfgString, "file:/")
+	path := strings.TrimPrefix(cfgString, "file/")
+	log.Debugln("File at ", path)
 
 	if err := util.FindOrCreateDisk(path, size); err != nil {
 		log.Errorln("Fail to find or create disk", err.Error())
 		return -C.EINVAL
 	}
-	state.file, err = os.Open(path)
+	state.file, err = os.OpenFile(path, os.O_RDWR, 0644)
 	if err != nil {
 		log.Errorln("Fail to open disk file", err.Error())
 		return -C.EINVAL
@@ -108,7 +126,71 @@ func handleRequest(dev *C.struct_tcmu_device, state *TcmuState) {
 }
 
 func handleCommand(dev *C.struct_tcmu_device, cmd *C.struct_tcmulib_cmd, state *TcmuState) int {
-	return C.SAM_STAT_GOOD
+	scsiCmd := C.tcmucmd_get_scsi_cmd(cmd)
+	switch scsiCmd {
+	case C.INQUIRY:
+		return int(C.tcmucmd_emulate_inquiry(cmd, dev))
+	case C.TEST_UNIT_READY:
+		return int(C.tcmucmd_emulate_test_unit_ready(cmd))
+	case C.SERVICE_ACTION_IN_16:
+		return int(C.tcmucmd_emulate_service_action_in(cmd, C.uint64_t(state.lbas), C.uint32_t(state.blockSize)))
+	case C.MODE_SENSE, C.MODE_SENSE_10:
+		return int(C.tcmucmd_emulate_mode_sense(cmd))
+	case C.MODE_SELECT, C.MODE_SELECT_10:
+		return int(C.tcmucmd_emulate_mode_select(cmd))
+	case C.READ_6, C.READ_10, C.READ_12, C.READ_16:
+		offset := int64(C.tcmucmd_get_lba(cmd)) * int64(state.blockSize)
+		length := int(C.tcmucmd_get_xfer_length(cmd)) * state.blockSize
+
+		buf := C.allocate_buffer(C.int(length))
+		if buf == nil {
+			log.Errorln("read failed: fail to allocate buffer")
+			return int(C.tcmucmd_set_medium_error(cmd))
+		}
+
+		goBuf := C.GoBytes(buf, C.int(length))
+		defer C.free(buf)
+		if readed, err := state.file.ReadAt(goBuf, offset); err != nil {
+			if readed != length || err != io.EOF {
+				log.Errorln("read failed: ", err.Error())
+				return int(C.tcmucmd_set_medium_error(cmd))
+			}
+		}
+
+		copied := C.tcmucmd_memcpy_into_iovec(cmd, buf, C.int(length))
+		if int(copied) != length {
+			log.Errorln("read failed: unable to complete buffer copy ")
+			return int(C.tcmucmd_set_medium_error(cmd))
+		}
+
+		return C.SAM_STAT_GOOD
+	case C.WRITE_6, C.WRITE_10, C.WRITE_12, C.WRITE_16:
+		offset := int64(C.tcmucmd_get_lba(cmd)) * int64(state.blockSize)
+		length := int(C.tcmucmd_get_xfer_length(cmd)) * state.blockSize
+
+		buf := C.allocate_buffer(C.int(length))
+		if buf == nil {
+			log.Errorln("read failed: fail to allocate buffer")
+			return int(C.tcmucmd_set_medium_error(cmd))
+		}
+		copied := C.tcmucmd_memcpy_from_iovec(cmd, buf, C.int(length))
+		if int(copied) != length {
+			log.Errorln("write failed: unable to complete buffer copy ")
+			return int(C.tcmucmd_set_medium_error(cmd))
+		}
+
+		goBuf := C.GoBytes(buf, C.int(length))
+		defer C.free(buf)
+		if _, err := state.file.WriteAt(goBuf, offset); err != nil {
+			log.Errorln("write failed: ", err.Error())
+			return int(C.tcmucmd_set_medium_error(cmd))
+		}
+
+		return C.SAM_STAT_GOOD
+	default:
+		log.Errorf("unknown command 0x%x\n", scsiCmd)
+	}
+	return C.TCMU_NOT_HANDLED
 }
 
 //export shClose
